37a60ef8254f4f21e9058cfba20f994748721e6a
J.P <70083705+stapmoshun@users.noreply.github.com>
2023-12-31 15:15:37 -0500

    add preexisting

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://gitlab.com/openconnect/openconnect/-/jobs/5809119325/artifacts/download?file_type=archive'
+
+$setupName = 'openconnect-installer-MinGW64-GnuTLS.exe'
+
+$packageArgs = @{
+    PackageName   = $env:ChocolateyPackageName
+    url           = $url
+    UnzipLocation = $toolsDir
+    fileType      = 'EXE'
+    Checksum      = 'BA844FBC912AC57FC2807BAD225A0A7F9623080C8E659F340FF8142BFA32A576'
+    ChecksumType  = 'sha256'
+    softwareName  = 'openconnect*'
+    silentArgs    = '/S'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$files = get-childitem $toolsDir -include openconnect-installer*.exe -recurse
+
+foreach ($file in $files) {
+  #generate an ignore file
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+$packageArgs.file = Join-Path -Path $toolsDir -ChildPath $setupName
+Install-ChocolateyInstallPackage @packageArgs
+
+Install-BinFile 'openconnect' "$($env:SystemDrive)\Program Files\OpenConnect\openconnect.exe"
+
+Install-BinFile 'list-system-keys' "$($env:SystemDrive)\Program Files\OpenConnect\list-system-keys.exe"
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿Uninstall-BinFile 'openconnect'
+Uninstall-ChocolateyPackage -PackageName 'openconnect' `
+                                -FileType 'EXE' `
+                                -SilentArgs "/S" `
+                                -File "$($env:SystemDrive)\Program Files\OpenConnect\Uninstall OpenConnect.exe"
